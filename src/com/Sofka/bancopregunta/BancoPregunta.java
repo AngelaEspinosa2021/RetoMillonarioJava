f218cb8dfa1dfdae079f4e0e12387d0d24742342
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-23 18:33:54 -0500

    Asignar he imprimir pregunta
    Se agregaron las funcionalidades para agregar he imprimir preguntas

--- src/com/Sofka/bancopregunta/BancoPregunta.java
+++ src/com/Sofka/bancopregunta/BancoPregunta.java
@@ -1,8 +1,10 @@
 package com.Sofka.bancopregunta;
 
 import java.util.Random;
+import java.util.prefs.BackingStoreException;
 
 public class BancoPregunta {
+
     protected String pregunta;
     protected String respuestaA;
     protected String respuestaB;
@@ -26,32 +28,51 @@ public class BancoPregunta {
     }
 
 
+    //calcular aleatorio
     public int numeroAleatorio(){
         Random random = new Random();
         return  random.nextInt(5) + 1;
     }
 
-    public void asignarPregunta(){
+    public BancoPregunta asignarPregunta(BancoPregunta preguntaUno,BancoPregunta preguntaDos,BancoPregunta preguntaTres,BancoPregunta preguntaCuatro,BancoPregunta preguntaCinco){
+        BancoPregunta bancoPregunta = new BancoPregunta();
         int numero = numeroAleatorio();
-        System.out.println(numero);
+        switch (numero){
+            case 1:
+                bancoPregunta=preguntaUno;
+                break;
+            case 2:
+                bancoPregunta=preguntaDos;
+                break;
+            case 3:
+                bancoPregunta=preguntaTres;
+                break;
+            case 4:
+                bancoPregunta=preguntaCuatro;
+                break;
+            case 5:
+                bancoPregunta=preguntaCinco;
+                break;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + numero);
+        }
+        return bancoPregunta;
+
     }
+
+    public String imprimirPregunta(BancoPregunta pregunta){
+      return pregunta.toString();
+
+    }
+
 
 
     @Override
     public String toString() {
-        return "BancoPregunta{" +
-                "pregunta='" + pregunta + '\'' +
-                ", respuestaA=" + respuestaA +
-                ", respuestaB=" + respuestaB +
-                ", respuestaC=" + respuestaC +
-                ", respuestaD=" + respuestaD +
-                ", puntaje=" + puntaje +
-                '}';
+        return "Juguemos por " + puntaje + " puntos" + "\n"
+                + pregunta + "\n" + respuestaA + "\n" + respuestaB +
+                "\n" + respuestaC + "\n" + respuestaD +
+                "\nEn Cualquier momento preciona R para retirarte";
     }
-
-    public static void main(String[] args) {
-        BancoPregunta bancoPregunta = new BancoPregunta();
-        bancoPregunta.asignarPregunta();
-    }
-
 }
--- src/com/Sofka/bancopregunta/PrimerNivel.java
+++ src/com/Sofka/bancopregunta/PrimerNivel.java
@@ -24,14 +24,16 @@ public class PrimerNivel extends BancoPregunta {
         BancoPregunta terceraPregunta = new PrimerNivel("La composicion quimica del agua es:", "A. HO2",
                 "B. 2H2O", "C. 2HO", "D. H2O", "D", 100);
 
-        BancoPregunta cuatroPregunta = new PrimerNivel("El autor de CIEN AÑOS DE SOLEDAD es:", "A. Gabriel Garcia Marquez",
+        BancoPregunta cuatroPregunta = new PrimerNivel("El autor de cien anos de soledad es:", "A. Gabriel Garcia Marquez",
                 "B. J.K. Rowling", "C. J.R.R. Tolkien", "D. David Sanchez Juliao", "A", 100);
 
         BancoPregunta cincoPregunta = new PrimerNivel("¿Nombre del libertador de Colombia?", "A. Cristobal Colon",
                 "B. Napoleon Bonaparte", "C. Simon Bolivar", "D. Diomedez Diaz", "C", 100);
 
-        return "";
+      return "";
+
     }
+
 }
 
 
