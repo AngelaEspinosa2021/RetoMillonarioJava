ab169ca66dff8828d49b702e9d4fab08a4e1bd25
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-23 16:17:49 -0500

    Agregando funcionalidad
    Se agrego la funcionalidad para la clase banco de pregunta

--- src/com/Sofka/bancopregunta/BancoPregunta.java
+++ src/com/Sofka/bancopregunta/BancoPregunta.java
@@ -1,4 +1,42 @@
 package com.Sofka.bancopregunta;
 
 public class BancoPregunta {
+    protected String pregunta;
+    protected String respuestaA;
+    protected String respuestaB;
+    protected String respuestaC;
+    protected String respuestaD;
+    protected String correcta;
+    protected double puntaje;
+
+    public BancoPregunta(){
+
+    }
+
+    public BancoPregunta(String pregunta, String respuestaA, String respuestaB, String respuestaC, String respuestaD, String correcta, double puntaje) {
+        this.pregunta = pregunta;
+        this.respuestaA = respuestaA;
+        this.respuestaB = respuestaB;
+        this.respuestaC = respuestaC;
+        this.respuestaD = respuestaD;
+        this.correcta=correcta;
+        this.puntaje = puntaje;
+    }
+
+
+
+
+
+    @Override
+    public String toString() {
+        return "BancoPregunta{" +
+                "pregunta='" + pregunta + '\'' +
+                ", respuestaA=" + respuestaA +
+                ", respuestaB=" + respuestaB +
+                ", respuestaC=" + respuestaC +
+                ", respuestaD=" + respuestaD +
+                ", puntaje=" + puntaje +
+                '}';
+    }
+
 }
