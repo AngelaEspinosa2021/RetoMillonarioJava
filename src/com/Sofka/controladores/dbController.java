124fefc0b5d4ec5611895d04f6e56af5c2628662
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-04-24 00:35:28 -0500

    Librería y Conexion a la DB: Agregada la libreria JDBC y la Clase de conexión a la base de datos

---
+++ src/com/Sofka/controladores/dbController.java
@@ -0,0 +1,23 @@
+package com.Sofka.controladores;
+
+import javax.swing.*;
+import java.sql.*;
+
+public class dbController {
+
+    Connection conexion;
+
+    public Connection conectar() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conexion = DriverManager.getConnection("jdbc:sqlite:src/com/Sofka/database/millonario.db");
+            if (conexion != null) {
+                JOptionPane.showInternalMessageDialog(null, "Conectado a la base de datos");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage().toString() + "No se pudo conectar a la base de datos");
+        }
+        return conexion;
+    }
+}
+
