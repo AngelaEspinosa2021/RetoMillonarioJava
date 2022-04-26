a296add3c0f89bc9e55828cd3b0a3c60b52df28b
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-25 20:04:52 -0500

    Clase cuarto Nivel
    Se crea clase cuarto nivel que implementa la interfaz IserviciosNivel

--- src/com/Sofka/domain/servicionivel/CuartoNivel.java
+++ src/com/Sofka/domain/servicionivel/CuartoNivel.java
@@ -1,4 +1,57 @@
 package com.Sofka.domain.servicionivel;
 
-public class CuartoNivel {
+import com.Sofka.domain.bancopregunta.BancoPregunta;
+
+import java.util.Scanner;
+
+public class CuartoNivel implements IServicioNivel{
+    @Override
+    public int nivel() {
+        Scanner usuario = new Scanner(System.in);
+        BancoPregunta bancoPreguntaCuatro = this.conexionAlbanco();
+        int validadcion = 0;
+        int comprobar = -1;
+        while (comprobar != 3) {
+            String respuesta = usuario.nextLine();
+            String resultado = bancoPreguntaCuatro.evaluarRespuesta(respuesta);
+            System.out.println(resultado);
+            validadcion = this.validarRepuesta(resultado);
+            switch (validadcion) {
+                case -1:
+                    comprobar = 1;
+                    bancoPreguntaCuatro.mostraInformacion();
+                    break;
+                default:
+                    comprobar = 3;
+                    break;
+            }
+        }
+        return validadcion;
+    }
+
+
+
+
+    @Override
+    public int validarRepuesta(String resultado) {
+        if (resultado.equalsIgnoreCase("Respuesta incorrecta")) {
+            return  0;
+        }
+        if (resultado.equalsIgnoreCase("El usuario se retira")) {
+            return  2;
+        }
+        if (resultado.equalsIgnoreCase("Respuesta correcta")) {
+            return  1;
+        }
+        return -1;
+
+    }
+
+    @Override
+    public BancoPregunta conexionAlbanco() {
+        BancoPregunta bancoPreguntaUno = new BancoPregunta();
+        bancoPreguntaUno.setCuartoNivel();
+        return bancoPreguntaUno;
+    }
+
 }
