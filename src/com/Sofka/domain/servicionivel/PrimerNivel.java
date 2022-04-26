e9ea85956a4d26ca7795f815872192591c515b4a
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-25 20:00:12 -0500

    Clase Segundo Nivel
    Se crea clase segundo nivel que implementa la interfaz IserviciosNivel

--- src/com/Sofka/domain/servicionivel/PrimerNivel.java
+++ src/com/Sofka/domain/servicionivel/PrimerNivel.java
@@ -55,4 +55,4 @@ public class PrimerNivel implements IServicioNivel{
 
 }
 
-}
+
--- src/com/Sofka/domain/servicionivel/SegundoNivel.java
+++ src/com/Sofka/domain/servicionivel/SegundoNivel.java
@@ -1,4 +1,58 @@
 package com.Sofka.domain.servicionivel;
 
-public class SegundoNivel {
+import com.Sofka.domain.bancopregunta.BancoPregunta;
+
+import java.util.Scanner;
+
+public class SegundoNivel implements IServicioNivel{
+
+    @Override
+    public int nivel() {
+
+        Scanner usuario = new Scanner(System.in);
+        BancoPregunta bancoPreguntaDos = this.conexionAlbanco();
+
+        int validadcion = -1;
+        int comprobar = -1;
+        while (comprobar != 3) {
+            String respuesta = usuario.nextLine();
+            String resultado = bancoPreguntaDos.evaluarRespuesta(respuesta);
+            System.out.println(resultado);
+            validadcion = this.validarRepuesta(resultado);
+            switch (validadcion) {
+                case -1:
+                    comprobar = 1;
+                    bancoPreguntaDos.mostraInformacion();
+                    break;
+                default:
+                    comprobar = 3;
+                    break;
+            }
+        }
+        return validadcion;
+    }
+
+
+    @Override
+    public int validarRepuesta(String resultado) {
+        if (resultado.equalsIgnoreCase("Respuesta incorrecta")) {
+            return  0;
+        }
+        if (resultado.equalsIgnoreCase("El usuario se retira")) {
+            return  2;
+        }
+        if (resultado.equalsIgnoreCase("Respuesta correcta")) {
+            return  1;
+        }
+        return -1;
+
+    }
+
+    @Override
+    public BancoPregunta conexionAlbanco() {
+        BancoPregunta bancoPreguntaUno = new BancoPregunta();
+        bancoPreguntaUno.setSegundoNivel();
+        return bancoPreguntaUno;
+    }
+
 }
